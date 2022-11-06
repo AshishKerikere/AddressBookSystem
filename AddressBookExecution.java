ced4d34e2564455e4db5e0bcc76ec6f55f7471e9
AshishKerikere <93969829+AshishKerikere@users.noreply.github.com>
2022-11-06 15:33:58 +0530

    Updated AddressBookExecution Class till UC6

--- AddressBookExecution.java
+++ AddressBookExecution.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
+import java.lang.String;
 public class AddressBookExecution {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        AddressBook addressBook1 = new AddressBook();
+        AddressBook addressBook1 = new AddressBook("Sample Address Book");
 
         addressBook1.addContact();// to demonstrate the method to add contact
         addressBook1.changeContactDetails();// to demonstrate the method to change details of a Particular contact
@@ -16,5 +17,16 @@ public class AddressBookExecution {
             System.out.println("Start Entering the details for " + (i + 1) + "Contact");
             addressBook1.addContact();
         }
+
+        //to demonstrate the creation of multiple Address Books
+        System.out.println("Enter the number of Address Books you want");
+        int number_ofAddressBooks = sc.nextInt();
+        AddressBook addressBook[] = new AddressBook[number_ofAddressBooks];
+        for(int i = 0; i < number_ofAddressBooks; i++){
+            System.out.println("Enter the name of " +(i+1) +"Address Book");
+            String bookName = sc.next();
+            addressBook[i] = new AddressBook(bookName);
+        }
+
     }
 }
