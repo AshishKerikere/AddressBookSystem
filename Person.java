cd265f4d63f99e5620e240d05de5e37df0a2eaf1
AshishKerikere <93969829+AshishKerikere@users.noreply.github.com>
2022-11-06 15:03:44 +0530

    Updated till UC2 for Person Class

--- Person.java
+++ Person.java
@@ -7,4 +7,18 @@ public class Person {
     String zip;
     String phone_Number;
     String email;
+
+   public Person(String first_Name, String last_Name, String address, String city,
+                 String state, String zip, String phone_Number, String email){
+
+       this.first_Name = first_Name;
+       this.last_Name = last_Name;
+       this.address = address;
+       this.city = city;
+       this.state = state;
+       this.zip = zip;
+       this.phone_Number = phone_Number;
+       this.email = email;
+
+   }
 }
