506f586cd6e0cf21ed0e7227b0c510c7e506a794
AshishKerikere <93969829+AshishKerikere@users.noreply.github.com>
2022-11-06 15:18:02 +0530

    Updated AddressBookExecution Class till UC4

--- AddressBookExecution.java
+++ AddressBookExecution.java
@@ -5,6 +5,7 @@ public class AddressBookExecution {
         AddressBook addressBook1 = new AddressBook();
 
         addressBook1.addContact();// to demonstrate the method to add contact
-        addressBook1.findContact();// to demonstrate the method to change details of a Particular contact
+        addressBook1.changeContactDetails();// to demonstrate the method to change details of a Particular contact
+        addressBook1.changeContactDetails();// to demonstrate the method to delete a Particular contact;
     }
 }
